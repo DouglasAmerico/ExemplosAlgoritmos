f4e58b5cf5ecc7cf93d5677c010a8fe420ed7f75
Douglas Américo <douglas@likesistemas.com.br>
2024-06-05 21:14:34 -0300

    tratando o objeto de leitura, caso não consiga criar o objeto (sugestão do vscode)

--- ExerciciosJava/BaseMiniCursoVetores.java
+++ ExerciciosJava/BaseMiniCursoVetores.java
@@ -5,40 +5,40 @@ public class BaseMiniCursoVetores {
 
    public static Integer[] valoresProva1(String nome[], Integer qtdAlunos){
         Integer prova1[] = new Integer[qtdAlunos];
-        Scanner leitor = new Scanner(System.in);
-        for (int i = 0; i < qtdAlunos; i++) {
-            do{
-                System.out.println("Por favor insira o valor da Prova 1 do Aluno "+nome[i]+": ");
-                prova1[i]= leitor.nextInt();
-            }while(prova1[i] > 100 || prova1[i] < 0);
+        try (Scanner leitor = new Scanner(System.in)) {
+            for (int i = 0; i < qtdAlunos; i++) {
+                do{
+                    System.out.println("Por favor insira o valor da Prova 1 do Aluno "+nome[i]+": ");
+                    prova1[i]= leitor.nextInt();
+                }while(prova1[i] > 100 || prova1[i] < 0);
+            }
         }
-        
         return prova1;
     }
     
     public static Integer[] valoresProva2(String nome[], Integer qtdAlunos){
         Integer prova2[] = new Integer[qtdAlunos];
-        Scanner leitor = new Scanner(System.in);
-        for (int i = 0; i < qtdAlunos; i++) {
-            do{
-                System.out.println("Por favor insira o valor da Prova 2 do Aluno "+nome[i]+": ");
-                prova2[i]= leitor.nextInt();
-            }while(prova2[i] > 100 || prova2[i] < 0);
+        try (Scanner leitor = new Scanner(System.in)) {
+            for (int i = 0; i < qtdAlunos; i++) {
+                do{
+                    System.out.println("Por favor insira o valor da Prova 2 do Aluno "+nome[i]+": ");
+                    prova2[i]= leitor.nextInt();
+                }while(prova2[i] > 100 || prova2[i] < 0);
+            }
         }
-        
         return prova2;
     }
     
     public static Integer[] trabalhosnotas(String nome[], Integer qtdAlunos){
         Integer trabalhos[] = new Integer[qtdAlunos];
-        Scanner leitor = new Scanner(System.in);
-        for (int i = 0; i < qtdAlunos; i++) {
-            do{
-                System.out.println("Por favor insira o valor dos trabalhos do Aluno "+nome[i]+": ");
-                trabalhos[i]= leitor.nextInt();
-            }while(trabalhos[i] > 100 || trabalhos[i] < 0);
+        try (Scanner leitor = new Scanner(System.in)) {
+            for (int i = 0; i < qtdAlunos; i++) {
+                do{
+                    System.out.println("Por favor insira o valor dos trabalhos do Aluno "+nome[i]+": ");
+                    trabalhos[i]= leitor.nextInt();
+                }while(trabalhos[i] > 100 || trabalhos[i] < 0);
+            }
         }
-        
         return trabalhos;
     }
     
@@ -65,31 +65,32 @@ public class BaseMiniCursoVetores {
       }
 
     public static void main(String[] args) {        
-        Scanner leitor = new Scanner(System.in);
-        System.out.println("Irá informar a nota de quantos alunos?");
-        Integer qtdAlunos = leitor.nextInt();
-        
-        String nomes[] = new String[qtdAlunos];
-        Integer prova1[] = new Integer[qtdAlunos];
-        Integer prova2[] = new Integer[qtdAlunos];
-        Integer trabalhos[] = new Integer[qtdAlunos];
-        Double somaNotas[] = new Double[qtdAlunos];
-        String alunosAprovados[] = new String[qtdAlunos];
-        
-        
-        for (int i = 0; i < qtdAlunos; i++) {
-            System.out.println("Por favor informa o nome do Aluno "+ (i+1)+": ");
-            nomes[i]= leitor.next();
-        }
-        
-        prova1 = valoresProva1(nomes,qtdAlunos);
-        prova2 = valoresProva2(nomes,qtdAlunos);
-        trabalhos = trabalhosnotas(nomes,qtdAlunos);
-        somaNotas = calculoDasNotas(prova1, prova2, trabalhos,qtdAlunos);
-        alunosAprovados = aprovados(somaNotas, alunosAprovados,qtdAlunos);
+        try (Scanner leitor = new Scanner(System.in)) {
+            System.out.println("Irá informar a nota de quantos alunos?");
+            Integer qtdAlunos = leitor.nextInt();
+            
+            String nomes[] = new String[qtdAlunos];
+            Integer prova1[] = new Integer[qtdAlunos];
+            Integer prova2[] = new Integer[qtdAlunos];
+            Integer trabalhos[] = new Integer[qtdAlunos];
+            Double somaNotas[] = new Double[qtdAlunos];
+            String alunosAprovados[] = new String[qtdAlunos];
+            
+            
+            for (int i = 0; i < qtdAlunos; i++) {
+                System.out.println("Por favor informa o nome do Aluno "+ (i+1)+": ");
+                nomes[i]= leitor.next();
+            }
+            
+            prova1 = valoresProva1(nomes,qtdAlunos);
+            prova2 = valoresProva2(nomes,qtdAlunos);
+            trabalhos = trabalhosnotas(nomes,qtdAlunos);
+            somaNotas = calculoDasNotas(prova1, prova2, trabalhos,qtdAlunos);
+            alunosAprovados = aprovados(somaNotas, alunosAprovados,qtdAlunos);
 
-        for (int i = 0; i < qtdAlunos; i++) {
-            System.out.println("O aluno: "+nomes[i]+" foi "+alunosAprovados[i]+" com a nota "+somaNotas[i]);
+            for (int i = 0; i < qtdAlunos; i++) {
+                System.out.println("O aluno: "+nomes[i]+" foi "+alunosAprovados[i]+" com a nota "+somaNotas[i]);
+            }
         }
     }   
 }
